23062e04a6ca6e7a9c969727d0e1f1734e9e7545
GaneshShihire <shihireganesh0@gmail.com>
2025-08-09 11:00:51 +0530

    Leetcode Jump Game Problem level medium

---
+++ Arrays/JumpGame.java
@@ -0,0 +1,14 @@
+public class JumpGame {
+
+    class Solution {
+    public boolean canJump(int[] nums) {
+        int maxreach=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i>maxreach) return false;
+            maxreach= Math.max(maxreach,i+nums[i]);
+        }
+        return true;
+    }
+}
+}
