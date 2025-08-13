d134815e91a795a762c297e0e8ee8e6b80c4c566
GaneshShihire <shihireganesh0@gmail.com>
2025-08-13 17:01:15 +0530

    Longpressed Name problem solved from leetcode

---
+++ StringArray/isLongPressedName.java
@@ -0,0 +1,33 @@
+package StringArray;
+
+public class isLongPressedName {
+
+    class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        
+        if(name.charAt(0) != typed.charAt(0)){
+            return false;
+        }
+        if(name.length() > typed.length()) {
+            return false;
+        }
+
+        int i = 1;
+        int j = 1;
+        while(j < typed.length()){
+            if(i < name.length() && name.charAt(i) == typed.charAt(j)){
+                i++;
+                j++;
+            }else if(typed.charAt(j) == typed.charAt(j-1)){
+                j++;
+            }else{
+                return false;
+            }
+
+        }
+        return i == name.length();
+
+        
+    }
+}
+}
