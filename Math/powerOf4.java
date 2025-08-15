42f40e99ea1df63de659440cd4ccd52e6823ff74
GaneshShihire <shihireganesh0@gmail.com>
2025-08-15 21:29:08 +0530

    Daily Problem 3 solved

---
+++ Math/powerOf4.java
@@ -0,0 +1,12 @@
+package Math;
+public class powerOf4 {
+
+    class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) return false;
+        while (n % 4 == 0) n /= 4;
+        return n == 1;
+    }
+}
+
+}
